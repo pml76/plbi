c2fb665602d6b489890e6a3a7e4fb53602a2ed81
pml76 <peter.lennartz@gmail.com>
2024-02-07 14:15:52 +0100

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-pub mod grammar;
 pub mod context;
+pub mod cxxqt_object;
 pub mod error;
+pub mod grammar;
+
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
+pub fn main2() {
+    // Create the application and engine
+    let mut app = QGuiApplication::new();
+    let mut engine = QQmlApplicationEngine::new();
+
+    // Load the QML path into the engine
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from("qrc:/qt/qml/com/kdab/cxx_qt/demo/qml/main.qml"));
+    }
+
+    // Start the app
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
+}
